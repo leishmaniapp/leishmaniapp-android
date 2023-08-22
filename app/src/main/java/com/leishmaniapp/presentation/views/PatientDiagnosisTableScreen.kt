a704e4491f835bd89e63dc2c55b7d97897d6e98f
Angel Talero <angelgotalero@outlook.com>
2023-08-22 16:19:12 -0500

    Started table screen

--- app/src/main/java/com/leishmaniapp/presentation/views/PatientDiagnosisTableScreen.kt
+++ app/src/main/java/com/leishmaniapp/presentation/views/PatientDiagnosisTableScreen.kt
@@ -1,2 +1,26 @@
 package com.leishmaniapp.presentation.views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.leishmaniapp.presentation.theme.LeishmaniappTheme
+
+@Composable
+fun PatientDiagnosisTableScreen() {
+    Scaffold { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PatientDiagnosisTableScreenPreview() {
+    LeishmaniappTheme {
+        PatientDiagnosisTableScreen()
+    }
+}
