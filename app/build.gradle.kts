7c8bb297478475b28f657f2c5e5d02f69af2803e
Ángel Talero <angelgotalero@outlook.com>
2025-09-18 22:12:59 -0500

    Added doc support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.parcelize)
